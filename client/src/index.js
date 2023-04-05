e3b0f226f58ba5198a5b2001395c28b74f4bd979
thenordplace <sadapan@gmail.com>
2023-04-04 21:34:10 -0600

    redid react app

---
+++ client/src/index.js
@@ -0,0 +1,13 @@
+// TODO: Add a comment giving a brief description of what React is
+//allows you to break things into components
+import React from "react";
+
+// TODO: Add a comment that describes the difference between react and react-dom
+//reactDOM
+import ReactDOM from "react-dom";
+
+import App from "./App";
+
+// TODO: Add a comment describing the significance of the ReactDOM.render method
+//turns the react element into actual DOM
+ReactDOM.render(<App />, document.getElementById("root"));
