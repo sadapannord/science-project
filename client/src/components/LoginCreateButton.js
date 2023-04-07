ef5363c3caec190d39f2ca5514420fd5c9e70375
MuchMuchierCoding <115940098+MuchMuchierCoding@users.noreply.github.com>
2023-04-06 18:51:29 -0600

    Added JSX framework for app.js and components for Homepage and Sign-in/up page (#5)

---
+++ client/src/components/LoginCreateButton.js
@@ -0,0 +1,10 @@
+function LoginCreateButton() {
+    return (
+    <div>
+        <button>Login</button>
+        <button>Sign Up</button>
+    </div>
+    );
+}
+
+export default LoginCreateButton;
