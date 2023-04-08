4acfe8f59cee70c00a2a38d989cad4970b6e2c78
thenordplace <sadapan@gmail.com>
2023-04-07 21:43:01 -0600

    got search form to work with API

--- client/src/components/SignUpForm.js
+++ client/src/components/SignUpForm.js
@@ -1,35 +1,83 @@
-function SignUpForm() {
-    return (
-        <div>
-            <form>
-                <label>
-                    First Name
-                    <input type = 'text' name = 'First Name'/>
-                </label>
-                <label>
-                    Last Name
-                    <input type = 'text' name = 'Last Name'/>
-                </label>
-                <label>
-                    Username
-                    <input type = 'text' name = 'Username'/>
-                </label>
-                <label>
-                    Email
-                    <input type = 'text' name = 'Email'/>
-                </label>
-                <label>
-                    Password
-                    <input type = 'text' name = 'Password'/>
-                </label>
-                <label>
-                    Confirm Password
-                    <input type = 'text' name = 'Confirm Password'/>
-                </label>
-                <input type = 'submit' value = 'Login/Create Account'/>
-            </form>
-        </div>
-    );
+// import { set } from "mongoose";
+import React, { useState } from "react";
+
+function SignUpForm(props) {
+  const [username, setUsername] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleInputChange = (e) => {
+    const { name, value } = e.target;
+    if (name === "Username") {
+      setUsername(value);
+    } else if (name === "Email") {
+      setEmail(value);
+    } else if (name === "Password") {
+      setPassword(value);
+    }
+  };
+
+  const handleFormSubmit = (e) => {
+    e.preventDefault();
+    alert(`Hello ${username}`);
+    setUsername("");
+    setEmail("");
+    setPassword("");
+  };
+
+  return (
+    <div>
+      <form>
+        {/* <label>
+          First Name
+          <input type="text" name="First Name" />
+        </label>
+        <label>
+          Last Name
+          <input type="text" name="Last Name" />
+        </label> */}
+        <label>
+          Username
+          <input
+            type="text"
+            name="Username"
+            onChange={handleInputChange}
+            value={username}
+            placeholder="Username"
+          />
+        </label>
+        <label>
+          Email
+          <input
+            type="text"
+            name="Email"
+            onChange={handleInputChange}
+            value={email}
+            placeholder="Email"
+          />
+        </label>
+        <label>
+          Password
+          <input
+            type="text"
+            name="Password"
+            onChange={handleInputChange}
+            value={password}
+            placeholder="Password"
+          />
+        </label>
+        {/* <label>
+          Confirm Password
+          <input type="text" name="Confirm Password" />
+        </label> */}
+        <input
+          type="submit"
+          value="Login/Create Account"
+          onClink={handleFormSubmit}
+        />
+      </form>
+    </div>
+  );
 }
 
 export default SignUpForm;
---
+++ client/src/utils/API.js
@@ -0,0 +1,46 @@
+import axios from "axios";
+
+const search = async (query) => {
+  const response = await axios.get(
+    `https://api.le-systeme-solaire.net/rest/bodies/:${query}`
+  );
+  return response.data;
+};
+
+//eslint-disable-next-line
+export default { search };
+
+// //bodies API call
+// const bodies = null;
+
+// const xhr = new XMLHttpRequest();
+// xhr.withCredentials = true;
+
+// xhr.addEventListener("readystatechange", function () {
+//   if (this.readyState === this.DONE) {
+//     console.log(this.responseText);
+//   }
+// });
+
+// xhr.open("GET", "https://api.astronomyapi.com/api/v2/bodies");
+// xhr.setRequestHeader("Authorization", process.env.APP_HASHED);
+
+// xhr.send(bodies);
+
+// //star chart call
+// const starCharts =
+//   '{"style":"default","observer":{"latitude":null,"longitude":null,"date":"2023-04-06"},"view":{"type":"constellation","parameters":{"constellation":"ori"}}}';
+
+// const xhr = new XMLHttpRequest();
+// xhr.withCredentials = true;
+
+// xhr.addEventListener("readystatechange", function () {
+//   if (this.readyState === this.DONE) {
+//     console.log(this.responseText);
+//   }
+// });
+
+// xhr.open("POST", "https://api.astronomyapi.com/api/v2/studio/star-chart");
+// xhr.setRequestHeader("Authorization", process.env.APP_HASHED);
+
+// xhr.send(starCharts);
