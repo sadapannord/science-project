ea8a83285bc411f43f978487aed026e8a385900b
Ying Yang <vieyrayessenia8@gmail.com>
2023-04-08 18:23:43 -0600

    added authentication, JWT, and User login code

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -4,12 +4,20 @@ const typeDefs = gql`
   type User {
     _id: ID
     username: String
-    name: String
     email: String
+    password: String
   }
+
+  type Auth {
+    token: ID!
+    user: User
+  }
+
   type Mutation {
-    addUser(username: String!, email: String!, password: String!): User
+    addUser(username: String!, email: String!, password: String!): Auth
+    login(email: String!, password: String!): Auth
   }
+  
   type Query {
     user: User
   }
