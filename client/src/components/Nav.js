35db7b65bf9ac61eb4e7f5c9c160de9c9066c52b
sadapannord <sadapan@gmail.com>
2023-04-08 20:29:01 -0600

    added some routes (#10)
    * added model and schemas
    
    * got query to work
    
    * getting API call
    
    * added second API call
    
    * got search form to work with API
    
    * added some routes

--- client/src/components/Nav.js
+++ client/src/components/Nav.js
@@ -1,12 +1,17 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import Auth from "../utils/auth";
+
 function Nav() {
-    return (
-        <nav>
-        <a>About</a>
-        <a>Start your Project!</a>
-        <h1>Spacetastic Science</h1>
-        <a>Login</a>
-        </nav>
-    )
+  return (
+    <nav>
+      <a>About</a>
+      <a>Start your Project!</a>
+      <h1>Spacetastic Science</h1>
+      <a>Login</a>
+      <Link to="/signup"> Signup</Link>
+    </nav>
+  );
 }
 
 export default Nav;
--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -9,38 +9,3 @@ const search = async (query) => {
 
 //eslint-disable-next-line
 export default { search };
-
-// //bodies API call
-// const bodies = null;
-
-// const xhr = new XMLHttpRequest();
-// xhr.withCredentials = true;
-
-// xhr.addEventListener("readystatechange", function () {
-//   if (this.readyState === this.DONE) {
-//     console.log(this.responseText);
-//   }
-// });
-
-// xhr.open("GET", "https://api.astronomyapi.com/api/v2/bodies");
-// xhr.setRequestHeader("Authorization", process.env.APP_HASHED);
-
-// xhr.send(bodies);
-
-// //star chart call
-// const starCharts =
-//   '{"style":"default","observer":{"latitude":null,"longitude":null,"date":"2023-04-06"},"view":{"type":"constellation","parameters":{"constellation":"ori"}}}';
-
-// const xhr = new XMLHttpRequest();
-// xhr.withCredentials = true;
-
-// xhr.addEventListener("readystatechange", function () {
-//   if (this.readyState === this.DONE) {
-//     console.log(this.responseText);
-//   }
-// });
-
-// xhr.open("POST", "https://api.astronomyapi.com/api/v2/studio/star-chart");
-// xhr.setRequestHeader("Authorization", process.env.APP_HASHED);
-
-// xhr.send(starCharts);
