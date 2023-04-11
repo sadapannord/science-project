e8aece5aafd4e09cb47b1c89382c42f5e3e80e5e
Hannah Edwards <44388330+hannahedwards@users.noreply.github.com>
2023-04-11 00:00:32 -0400

    Hannah (#14)
    * tailwind connection
    
    * added styling
    
    * package.json update
    
    ---------
    
    Co-authored-by: Hannah Edwards <hannahedwards@MacBook-Pro-2.local>
    Co-authored-by: sadapannord <sadapan@gmail.com>

--- client/src/components/Nav.js
+++ client/src/components/Nav.js
@@ -3,15 +3,21 @@ import { Link } from "react-router-dom";
 import Auth from "../utils/auth";
 
 function Nav() {
-  return (
-    <nav>
-      <a>About</a>
-      <a>Start your Project!</a>
-      <h1>Spacetastic Science</h1>
-      <a>Login</a>
-      <Link to="/signup"> Signup</Link>
-    </nav>
-  );
+    return (
+        <nav className="bg-purple-400 justify-between flex text-white">
+            <div className="justify-items-start my-7">
+                <a className="px-3">About</a>
+                <a className="px-3">Start your Project!</a>
+            </div>
+            <div className="my-7">
+            <h1 className="justify-items-center text-3xl font-bold underline">Spacetastic Science</h1>
+            </div>
+            <div className="justify-items-end my-7">
+                <a className="px-3">Login</a>
+                <Link className="px-3" to="/signup"> Signup</Link>
+            </div>
+        </nav>
+    );
 }
 
 export default Nav;
