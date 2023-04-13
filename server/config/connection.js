d167f5da984f1cfe7e57e6bd36ffbd646166df36
sadapannord <sadapan@gmail.com>
2023-04-13 12:29:07 -0600

    mongo connection (#21)
    * added model and schemas
    
    * got query to work
    
    * getting API call
    
    * added second API call
    
    * got search form to work with API
    
    * added some routes
    
    * adding queries
    
    * attempting to render data from search
    
    * got query working for users
    
    * attempting to get front end to create user
    
    * changed route to signup
    
    * adding 2nd API back in
    
    * added style to signup

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,12 +1,12 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
-mongoose.connect('mongodb://127.0.0.1:27017/techmatchup', {
-  // mongoose.connect("mongodb+srv://amandapaul1223:Cl!mber2008@cluster0.xmvkizb.mongodb.net/techmatchup", {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-});
+mongoose.connect(
+  process.env.MONGO_URL || "mongodb://127.0.0.1:27017/techmatchup",
+  {
+    // mongoose.connect("mongodb+srv://amandapaul1223:Cl!mber2008@cluster0.xmvkizb.mongodb.net/techmatchup", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  }
+);
 
 module.exports = mongoose.connection;
-
-
-
