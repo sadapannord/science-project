28b993fca510f67d8c6fc6038ee80da37915dd2c
Hannah Edwards <hannahedwards@MacBook-Pro-2.local>
2023-04-13 00:47:28 -0400

    hero banner update

--- client/src/components/Hero.js
+++ client/src/components/Hero.js
@@ -7,23 +7,17 @@ function Hero() {
   return (
     <div>
       <div className="relative">
-        <h2 className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-center text-white">
+        <h2 className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-center text-white text-3xl bg-purple-300/50  border-solid border-2 border-purple-300 rounded-lg">
           Begin your search for space facts to make the coolest science project
           ever!
         </h2>
         <img src={spaceBackground} alt="background" />
       </div>
-      <button>Create Account</button>
+      {/* <button>Create Account</button>
       <SolarBodiesSearch />
-      <Examples />
+      <Examples /> */}
     </div>
   );
 }
 
 export default Hero;
-
-
-
-// <div className='bg-black/30 absolute top-0 left-0 w-full h-screen' />
-// <div className='absolute text-2xl md:text-7xl text-white top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2'>
-//   <p className='tex-red-700'>I am Morgan Freeman</p>
