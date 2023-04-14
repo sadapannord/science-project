74c0fb5a73e49aec81abec7d2b83d3cbb53c2303
na-br-wo <nathan.b.wood@gmail.com>
2023-04-13 21:22:45 -0600

    adding CSS styling to login button with <link>

--- client/src/pages/Signup.js
+++ client/src/pages/Signup.js
@@ -6,6 +6,7 @@ import { useMutation } from "@apollo/client";
 import { ADD_USER } from "../utils/mutations";
 import Auth from "../utils/auth";
 import spaceBackground from "../images/space.jpg";
+import { Link } from "react-router-dom";
 
 function SignUpForm(props) {
   const [formState, setFormState] = useState({
@@ -45,14 +46,6 @@ function SignUpForm(props) {
           className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-center text-white text-3xl bg-purple-300/50  border-solid border-2 border-purple-300 rounded-lg"
           onSubmit={handleFormSubmit}
         >
-          {/* <label>
-          First Name
-          <input type="text" name="First Name" />
-        </label>
-        <label>
-          Last Name
-          <input type="text" name="Last Name" />
-        </label> */}
           <label>
             Username
             <input
@@ -60,7 +53,6 @@ function SignUpForm(props) {
               type="username"
               name="username"
               onChange={handleInputChange}
-              // value={formState.username}
               placeholder="Username"
             />
           </label>
@@ -71,7 +63,6 @@ function SignUpForm(props) {
               type="email"
               name="email"
               onChange={handleInputChange}
-              // value={formState.email}
               placeholder="Email"
             />
           </label>
@@ -82,21 +73,10 @@ function SignUpForm(props) {
               type="password"
               name="password"
               onChange={handleInputChange}
-              // value={formState.password}
               placeholder="Password"
             />
           </label>
-          {/* <label>
-          Confirm Password
-          <input type="text" name="Confirm Password" />
-        </label> */}
-          <input
-            type="submit"
-            value="Login/Create Account"
-            // onClick={() => {
-            //   handleFormSubmit();
-            // }}
-          />
+          <Link className="px-3 p-2 m-2 border-solid border-2 border-purple-300 rounded-lg" to="/project" type='submit'> Login/Create Account</Link>
         </form>
         <img src={spaceBackground} alt="background" />
       </div>
