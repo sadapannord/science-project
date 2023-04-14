342b6c4f04dc633cb2886928d395da3aaa9986d2
Yessenia Vieyra <115037851+Yvieyra@users.noreply.github.com>
2023-04-13 19:34:17 -0600

    Yessenia (#23)
    * created User model
    
    * added User schemas and seedsData
    
    * edited resolvers.js
    
    * added authentication, JWT, and User login code
    
    * edited auth. file for correct expiration code
    
    * solved bcrpt bug, added Project model,  updated resovlers and typedefs
    
    * edited typeDefs, current query bug
    
    * closer to creating database connection, still need to add user to DB
    
    * resolved database connection with graphQL
    
    * edited Signup j file and Project js
    
    ---------
    
    Co-authored-by: sadapannord <sadapan@gmail.com>

--- client/src/pages/Signup.js
+++ client/src/pages/Signup.js
@@ -1,38 +1,34 @@
 // import { set } from "mongoose";
 import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
-
 import { useMutation } from "@apollo/client";
 import { ADD_USER } from "../utils/mutations";
 import Auth from "../utils/auth";
 import spaceBackground from "../images/space.jpg";
-// import Project from "./Project";
+import { Link } from 'react-router-dom';
 
+// import Project from "./Project";
 function SignUpForm(props) {
+  const navigate = useNavigate();
   const [formState, setFormState] = useState({
     username: "",
     email: "",
     password: "",
   });
   const [addUser] = useMutation(ADD_USER);
-
   const HandleFormSubmit = async (event) => {
     event.preventDefault();
-    const navigate = useNavigate();
     console.log(formState);
-
     try {
       const { data } = await addUser({
         variables: { ...formState },
       });
-
       Auth.login(data.addUser.token);
-      navigate("/project");
+      // navigate("/project");
     } catch (e) {
       console.error(e);
     }
   };
-
   const handleInputChange = (event) => {
     const { name, value } = event.target;
     setFormState({
@@ -40,7 +36,6 @@ function SignUpForm(props) {
       [name]: value,
     });
   };
-
   return (
     <div>
       <div className="relative">
@@ -93,12 +88,12 @@ function SignUpForm(props) {
           Confirm Password
           <input type="text" name="Confirm Password" />
         </label> */}
-          <input type="submit" value="Login/Create Account" />
+          {/* <input type="submit" value="Login/Create Account" /> */}
+          <Link className="px-3 p-2 m-2 border-solid border-2 border-purple-300 rounded-lg" to="/project" type='submit'> Login/Create Account</Link>
         </form>
         <img src={spaceBackground} alt="background" />
       </div>
     </div>
   );
 }
-
 export default SignUpForm;
