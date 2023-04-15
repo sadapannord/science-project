ea8cf2a80944dc7d26c2b3646888772f8c8a2f8d
Ying Yang <vieyrayessenia8@gmail.com>
2023-04-14 21:07:27 -0600

    most recent code

--- server/utils/auth.js
+++ server/utils/auth.js
@@ -1,6 +1,6 @@
 const jwt = require("jsonwebtoken");
 
-const secret = 'mysecretssshhhhhhh'; //env.variable?
+const secret = 'mysecretssshhhhhhh'; //env.variable??
 const expiration = "2h";
 
 // module.exports = {
