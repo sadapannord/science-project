3f455487518e49236ab9dc46d01cfa5816559cc2
Yessenia Vieyra <115037851+Yvieyra@users.noreply.github.com>
2023-04-14 20:45:43 -0600

    Yessenia (#31)
    * created User model
    
    * added User schemas and seedsData
    
    * edited resolvers.js
    
    * added authentication, JWT, and User login code
    
    * edited auth. file for correct expiration code
    
    * solved bcrpt bug, added Project model,  updated resovlers and typedefs
    
    * edited typeDefs, current query bug
    
    * closer to creating database connection, still need to add user to DB
    
    * resolved database connection with graphQL
    
    * edited Signup j file and Project js
    
    * resolved addUser to DB issue, working on authentication login
    
    * commented out login page to change content and added /login to nav.js
    
    ---------
    
    Co-authored-by: sadapannord <sadapan@gmail.com>

--- client/src/pages/Signup.js
+++ client/src/pages/Signup.js
@@ -1,30 +1,23 @@
 // import { set } from "mongoose";
 import React, { useState } from "react";
+import { useNavigate} from "react-router-dom";
+
 import { useMutation } from "@apollo/client";
 import { ADD_USER } from "../utils/mutations";
 import Auth from "../utils/auth";
 import spaceBackground from "../images/space.jpg";
-import { Link } from "react-router-dom";
+
 
 function SignUpForm(props) {
+  const navigate = useNavigate();
   const [formState, setFormState] = useState({
-    username: "",
-    email: "",
-    password: "",
+    username: '',
+    email: '',
+    password: ''
   });
+
   const [addUser] = useMutation(ADD_USER);
-  const HandleFormSubmit = async (event) => {
-    event.preventDefault();
-    console.log("is this working?");
-    try {
-      const { data } = await addUser({
-        variables: { ...formState },
-      });
-      Auth.login(data.addUser.token);
-    } catch (e) {
-      console.error(e);
-    }
-  };
+
   const handleInputChange = (event) => {
     const { name, value } = event.target;
     setFormState({
@@ -33,12 +26,31 @@ function SignUpForm(props) {
     });
   };
 
+  const handleFormSubmit = async (event) => {
+    event.preventDefault();
+    
+    navigate('/project');
+    console.log(formState);
+
+    try {
+      const { data } = await addUser({
+        variables: { ...formState }
+      });
+
+      Auth.login(data.addUser.token);
+
+    } catch (e) {
+      console.error(e)
+    }
+  };
+
+
   return (
     <div>
       <div className="relative">
         <form
           className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-center text-white text-3xl bg-purple-300/50  border-solid border-2 border-purple-300 rounded-lg"
-          onSubmit={HandleFormSubmit}
+          onSubmit={handleFormSubmit}
         >
           <label>
             Username
@@ -70,18 +82,21 @@ function SignUpForm(props) {
               placeholder="Password"
             />
           </label>
-
-          <Link
-            className="px-3 p-2 m-2 border-solid border-2 border-purple-300 rounded-lg"
-            to="/project"
+          {/* <label>
+          Confirm Password
+          <input type="text" name="Confirm Password" />
+        </label> */}
+          <input
             type="submit"
-          >
-            Login/Create Account
-          </Link>
-        </form>
+            value="Login/Create Account"
+          />
+          {/* <Link className="px-3 p-2 m-2 border-solid border-2 border-purple-300 rounded-lg" to="/project" type="submit"> Login/Create Account</Link> */}
+          
+        </form >
         <img src={spaceBackground} alt="background" />
       </div>
     </div>
   );
 }
+
 export default SignUpForm;
