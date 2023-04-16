6b968fa06083ad9f151a14e9f7499caa6f74d932
Five Pandas <amandapaul1223@gmail.com>
2023-04-16 16:30:42 -0600

    Planet information

---
+++ client/src/components/Earth.js
@@ -0,0 +1,34 @@
+function Earth() {
+    return (
+        <div className="">
+        <ul>
+            <li>
+            <p>My name is Earth!</p>
+            </li>
+            <li>           
+            <p>Here are some cool facts about me. I have:</p>
+            </li>
+            <li>
+            <p>- 1 moon named La Lune</p>
+            </li>
+            <li>
+            <p>- a density of 5.5136 10<sup>n</sup></p>
+            </li>
+            <li>           
+            <p>- a surface gravity is 9.8 m.s<sup>-2</sup></p>
+            </li>
+            <li>           
+            <p>- an average temperature of 288<sup>oK</sup></p>
+            </li>
+            <li>           
+            <p>- an axial tilt of 23.4393<sup>o</sup></p>
+            </li>
+            <li>           
+            <p>- a mean radius of 6371 kilometers</p>
+            </li>
+        </ul>
+        </div>
+    );
+}
+
+export default Earth;
---
+++ client/src/components/Uranus.js
@@ -0,0 +1,34 @@
+function Uranus() {
+    return (
+        <div className="">
+        <ul>
+            <li>
+            <p>My name is Uranus!</p>
+            </li>
+            <li>           
+            <p>Here are some cool facts about me. I have:</p>
+            </li>
+            <li>
+            <p>- 27 moons</p>
+            </li>
+            <li>
+            <p>- a density of 1.27 10<sup>n</sup></p>
+            </li>
+            <li>           
+            <p>- a surface gravity is 8.87 m.s<sup>-2</sup></p>
+            </li>
+            <li>           
+            <p>- an average temperature of 76<sup>oK</sup></p>
+            </li>
+            <li>           
+            <p>- an axial tilt of 97.77<sup>o</sup></p>
+            </li>
+            <li>           
+            <p>- a mean radius of 25,362 kilometers</p>
+            </li>
+        </ul>
+        </div>
+    );
+}
+
+export default Uranus;
