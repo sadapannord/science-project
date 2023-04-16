93d04acb8dcb181993f66eb5eb133a6d4d678de7
MuchMuchierCoding <115940098+MuchMuchierCoding@users.noreply.github.com>
2023-04-16 16:40:25 -0600

    Planet information (#56)

---
+++ client/src/components/Mercury.js
@@ -0,0 +1,34 @@
+function Mercury() {
+    return (
+        <div className="">
+        <ul>
+            <li>
+            <p>My name is Mercury!</p>
+            </li>
+            <li>           
+            <p>Here are some cool facts about me. I have:</p>
+            </li>
+            <li>
+            <p>- 0 moons</p>
+            </li>
+            <li>
+            <p>- a density of 5.4291 10<sup>n</sup></p>
+            </li>
+            <li>           
+            <p>- a surface gravity is 3.7 m.s<sup>-2</sup></p>
+            </li>
+            <li>           
+            <p>- an average temperature of 440<sup>oK</sup></p>
+            </li>
+            <li>           
+            <p>- an axial tilt of 0.0352<sup>o</sup></p>
+            </li>
+            <li>           
+            <p>- a mean radius of 2,439 kilometers</p>
+            </li>
+        </ul>
+        </div>
+    );
+}
+
+export default Mercury;
