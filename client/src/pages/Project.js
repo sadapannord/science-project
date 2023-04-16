39db3f476a11a162cfd1e4825f935379448c80e4
Hannah Edwards <hannahedwards@MacBook-Pro-2.local>
2023-04-16 11:49:47 -0400

    project title is populating

--- client/src/pages/Project.js
+++ client/src/pages/Project.js
@@ -9,9 +9,10 @@ function CreateProject(props) {
     title: "",
     notes: "",
   });
+  const [projects, setProjects] = useState([]); // new state variable to hold projects
   const [addProject] = useMutation(ADD_PROJECT);
 
-  const HandleFormSubmit = async (event) => {
+  const handleFormSubmit = async (event) => {
     event.preventDefault();
     console.log(formState);
 
@@ -19,6 +20,8 @@ function CreateProject(props) {
       const { data } = await addProject({
         variables: { ...formState },
       });
+      setProjects([...projects, data.addProject]); // add new project to the projects array
+      setFormState({ title: "", notes: "" }); // clear the form
     } catch (e) {
       console.error(e);
     }
@@ -32,50 +35,55 @@ function CreateProject(props) {
     });
   };
 
+
   return (
     <div>
-      <div className="relative">
-        <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-center text-white text-3xl bg-purple-300/50  border-solid border-2 border-purple-300 rounded-lg">
-          <div>
-            <div className="px-3 p-2 m-2 border-solid border-2 border-purple-200 rounded-lg bg-purple-300" >
-              <h1>What Planet would you like to research?: </h1>
-              <Planets />
-            </div>
-          </div>
-          <form className="border-solid border-2 border-purple-300 rounded-lg m-2 p-2 bg-purple-300" onSubmit={HandleFormSubmit}>
-            <label>
-              Project Title:
-              <input
-                className="text-black px-3 p-2 m-2 border-solid border-2 border-purple-200 rounded-lg"
-                type="title"
-                name="title"
-                onChange={handleInputChange}
-                placeholder="Project Title"
-              />
-            </label>
-            <br></br>
-            <label>
-              Notes:
-              <input
-                className="text-black px-3 p-2 m-2 border-solid border-2 border-purple-200 rounded-lg"
-                type="notes"
-                name="notes"
-                onChange={handleInputChange}
-                placeholder="Notes"
-              />
-            </label>
-            <div>
-              <input className="px-3 p-2 m-2 border-solid border-2 border-purple-200 rounded-lg" type="submit" value="Start New Project" />
-            </div>
-          </form>
-          <div className="border-solid border-2 border-purple-200 rounded-lg m-1 p-1 bg-purple-300">
-            this is where your new project wants to go
+    <div className="relative">
+      <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-center text-white text-3xl bg-purple-300/50  border-solid border-2 border-purple-300 rounded-lg">
+        <div>
+          <div className="px-3 p-2 m-2 border-solid border-2 border-purple-200 rounded-lg bg-purple-300" >
+            <h1>What Planet would you like to research?: </h1>
+            <Planets />
           </div>
         </div>
-        <img className="bg-no-repeat bg-cover h-screen w-screen" src={spaceBackground} alt="background" />
+        <form className="border-solid border-2 border-purple-300 rounded-lg m-2 p-2 bg-purple-300" onSubmit={handleFormSubmit}>
+          <label>
+            Project Title:
+            <input
+              className="text-black px-3 p-2 m-2 border-solid border-2 border-purple-200 rounded-lg"
+              type="title"
+              name="title"
+              value={formState.title} // bind value to state variable
+              onChange={handleInputChange}
+              placeholder="Project Title"
+            />
+          </label>
+          <br></br>
+          <label>
+            Notes:
+            <input
+              className="text-black px-3 p-2 m-2 border-solid border-2 border-purple-200 rounded-lg"
+              type="notes"
+              name="notes"
+              value={formState.notes} // bind value to state variable
+              onChange={handleInputChange}
+              placeholder="Notes"
+            />
+          </label>
+          <div>
+            <input className="px-3 p-2 m-2 border-solid border-2 border-purple-200 rounded-lg" type="submit" value="Start New Project" />
+          </div>
+        </form>
+        <div className="border-solid border-2 border-purple-200 rounded-lg m-1 p-1 bg-purple-300">
+          {projects.map((project) => (
+            <div key={project._id}>{project.title}</div>
+          ))}
+        </div>
       </div>
+      <img className="bg-no-repeat bg-cover h-screen w-screen" src={spaceBackground} alt="background" />
     </div>
-
+  </div>
   );
 }
 export default CreateProject;
+
